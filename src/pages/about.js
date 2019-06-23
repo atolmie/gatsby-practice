9c5ad750533d45d221a9a2651188ccf77be437d2
atolmie <atolmie22@gmail.com>
2019-06-23 11:52:12 -0500

    init

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `olive` }}>
+    <Header headerText="About Me" /> 
+    <p> Hello, my name is Anne Tolmie. I enjoy backpacking, travel, cooking, gardening, and working out. Of course, I also love to code.</p>
+  </div>
+)
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <h1>Hello!</h1>
+    <p>This is pretty neat.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
+)
