8ab304b4f029e8f88865e7b3a617f42b77823bc8
atolmie <atolmie22@gmail.com>
2019-07-17 16:40:50 -0500

    props example

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,8 @@ import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `olive` }}>
-    <Header headerText="About Me" /> 
+    <Header headerText="About Me" arbitraryPhrase="is arbitrary"/> 
+    <Header headerText="It's pretty cool" /> 
     <p> Hello, my name is Anne Tolmie. I enjoy backpacking, travel, cooking, gardening, and working out. Of course, I also love to code.</p>
   </div>
 )
